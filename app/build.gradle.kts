5dffedd1ce6e4bab3fa0cd623bd9a8ee40ecb89b
admin <davidparrra13@gmail.com>
2025-09-14 21:23:51 -0500

    ajustes en estilos del la navegación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,17 +26,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
